9b7df08e88306ff9e95184eb580c3ef89855e867
Gideon <gideonam@yahoo.com.br>
2025-08-13 23:21:08 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+from pygame.examples.grid import WINDOW_WIDTH, WINDOW_HEIGHT
+#c
+COLOR_ORANGE = (255, 165, 0)  # RGB para laranja
+COLOR_WHITE = (255, 255, 255)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+             'NEW GAME 2P - COOPERATIVE',
+             'NEW GAME 2P - COMPETITIVE',
+             'SCORE',
+             'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
